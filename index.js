0634dda562e999a77a13d73f65079b71461f8d33
Bill <billfeng2002@gmail.com>
2021-07-23 14:01:56 -0700

    commit tree 8dcc3d27dad32574acf1acb7a65b27ca56bd7d58

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Add your code here
+function submitData(name, email){
+    let configurationObject={
+        method: "POST",
+        headers:{
+            'Content-Type': 'application/json',
+            'Accept':'application/json'
+        },
+        body:JSON.stringify({
+            name: name,
+            email: email
+        })
+    }
+    return fetch('http://localhost:3000/users', configurationObject).then(response=> response.json())
+        .then(json => {
+            let id= json.id
+            document.body.append(id)
+        })
+        .catch( error=> {
+            document.body.append(error)
+        })
+}
